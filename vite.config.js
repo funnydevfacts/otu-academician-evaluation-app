670a3158702b94ade07903d4980179da85272893
Umuthan Özel <ozelumuthan@gmail.com>
2024-03-04 11:54:26 +0300

    Add directory aliases for easier importing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@components", replacement: path.resolve(__dirname, "src/components") },
+      { find: "@context", replacement: path.resolve(__dirname, "src/context") },
+      { find: "@forms", replacement: path.resolve(__dirname, "src/forms") },
+      { find: "@views", replacement: path.resolve(__dirname, "src/views") },
+    ],
+  },
 })
